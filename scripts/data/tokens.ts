ca750e3bd3ee603bbd2a4e837ec8ac210b006cd9
Røbėrtø <106074508+EchoDex@users.noreply.github.com>
2024-11-06 17:28:24 +0400

    feat: release for mainnet

--- scripts/data/tokens.ts
+++ scripts/data/tokens.ts
@@ -2,22 +2,14 @@ import { Token } from "../types";
 
 const data: Token[] = [
   {
-    name: "Tremp",
-    symbol: "TRMP",
-    decimals: 9,
+    name: "USDC",
+    symbol: "USDC",
+    decimals: 6,
     assetId:
-      "0x0b2d808a898cdae8b8661d398a98f8ff45e1e0f536ba2e498f6c7e53a71932cd",
-    priceFeed: "",
-    precision: 9,
-  },
-  {
-    name: "Kemele",
-    symbol: "KMLA",
-    decimals: 9,
-    assetId:
-      "0x368f9275e7d072794527b57d5b54688300008a400f41d926a013195e7074029c",
-    priceFeed: "",
-    precision: 9,
+      "0x286c479da40dc953bddc3bb4c453b608bba2e0ac483b077bd475174115395e6b",
+    priceFeed:
+      "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
+    precision: 2,
   },
   {
     name: "Ethereum",
@@ -28,36 +20,6 @@ const data: Token[] = [
     priceFeed:
       "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
     precision: 9,
-  },
-  {
-    name: "Ethereum",
-    symbol: "tETH",
-    decimals: 9,
-    assetId:
-      "0xf169e13e98ae8908199148380684894458b7916f074b85ebad2aaad489ce0d54",
-    priceFeed:
-      "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
-    precision: 9,
-  },
-  {
-    name: "Bitcoin",
-    symbol: "tBTC",
-    decimals: 8,
-    assetId:
-      "0x0dc8cdbe2798cb45ebc99180afc0bc514ffb505a80f122004378955c1d23892c",
-    priceFeed:
-      "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
-    precision: 8,
-  },
-  {
-    name: "USDC",
-    symbol: "tUSDC",
-    decimals: 6,
-    assetId:
-      "0x22dfb618b9fc621a7d53f0f599dd427fb5688e280062a8de8883a27819d3f276",
-    priceFeed:
-      "0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a",
-    precision: 2,
   },
 ];
 
