bbc14a28dad11a4a4342d66a6547cd0dfb420eb6
Røbėrtø <106074508+EchoDex@users.noreply.github.com>
2024-11-07 15:01:01 +0400

    feat: update indexer url

--- scripts/utils/getIndexerInfo.ts
+++ scripts/utils/getIndexerInfo.ts
@@ -6,8 +6,8 @@ export const getIndexerInfo = (
 ): Record<string, Indexer> => {
   return markets.reduce((prev, market) => {
     prev[market.contractId] = {
-      httpUrl: `https://indexer.dev.hyperindex.xyz/${indexerId}/v1/graphql`,
-      wsUrl: `wss://indexer.dev.hyperindex.xyz/${indexerId}/v1/graphql`,
+      httpUrl: `https://indexer.hyperindex.xyz/${indexerId}/v1/graphql`,
+      wsUrl: `wss://indexer.hyperindex.xyz/${indexerId}/v1/graphql`,
     } as Indexer;
 
     return prev;
